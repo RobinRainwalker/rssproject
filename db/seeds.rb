c850bb568cd0e032571355f56dae36c8bb54f083
RobinRainwalker <robin.rainwalker@gmail.com>
2017-02-25 14:02:54 -0500

    'done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,4 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times {Item.create!(source: "Item", description: "some thign")}
